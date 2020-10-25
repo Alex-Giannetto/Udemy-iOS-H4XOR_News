df9067b47c702828a9382787432498ff703127c7
Alexandre Giannetto <32279162+Alexg78bis@users.noreply.github.com>
2020-10-25 22:19:47 +0100

    Initial Commit

---
+++ Udemy-iOS-H4XOR_News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udemy-iOS-H4XOR_News
+//
+//  Created by Alex on 25/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udemy-iOS-H4XOR_News/Udemy_iOS_H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udemy_iOS_H4XOR_NewsApp.swift
+//  Udemy-iOS-H4XOR_News
+//
+//  Created by Alex on 25/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Udemy_iOS_H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
